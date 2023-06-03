a05250ff4794455ef341aa1019bcd81d1a6162fd
Frank Willian <frank19valentim@gmail.com>
2023-06-03 17:35:25 -0300

    Codigo da tela principal

---
+++ Telas/Jogo.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+const Game = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (i) => {
+    return (
+      <button className="square" onClick={() => handleClick(i)}>
+        {squares[i]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Vencedor: " + winner;
+  } else if (!squares.includes(null)) {
+    status = "Empate!";
+  } else {
+    status = "Próximo jogador: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
